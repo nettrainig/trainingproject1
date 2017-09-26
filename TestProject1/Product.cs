842db18a7ee0ad54962aad7e0a458f9444698f41
nettrainig <guthubtest@i.ua>
2017-09-26 15:01:49 +0300

    Task 1

---
+++ TestProject1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    class Product: IComparable
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+        public int CompareTo(object obj)
+        {
+            return string.Compare(Code, (obj as Product).Code);
+        }
+    }
+}
--- TestProject1/Program.cs
+++ TestProject1/Program.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 namespace TestProject1
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world! test");
+            List<Product> products = new List<Product>() { new Product { ID = 1, Code = "B" }, new Product { ID = 2, Code = "A"}, new Product { ID = 3, Code = "C"} };
+            products.Sort();
+            foreach (var product in products)
+                Console.WriteLine(product.Code);            
             Console.ReadLine();
         }
     }
