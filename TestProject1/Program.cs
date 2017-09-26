04ffa7f874689d749b96c767763f28881f93c897
nettrainig <guthubtest@i.ua>
2017-09-26 14:14:23 +0300

    test commit

--- TestProject1/Program.cs
+++ TestProject1/Program.cs
@@ -10,7 +10,7 @@ namespace TestProject1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello, world! test");
             Console.ReadLine();
         }
     }
