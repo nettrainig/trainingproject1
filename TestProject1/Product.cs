36e12755c78d347ab51398f4a1cf1279b4ee475a
nettrainig <guthubtest@i.ua>
2017-09-27 13:57:39 +0300

    Task 2

--- TestProject1/Product.cs
+++ TestProject1/Product.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace TestProject1
 {
-    class Product: IComparable
-    {
+    public class Product : IComparable
+    {        
         public int ID { get; set; }
+
+        [UseForEqualityCheck]
         public string Code { get; set; }
+
         public string Name { get; set; }
+
         public Decimal Price { get; set; }
+
         public int CompareTo(object obj)
         {
             return string.Compare(Code, (obj as Product).Code);
--- TestProject1/Program.cs
+++ TestProject1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using log4net;
 using System.Threading.Tasks;
 
 namespace TestProject1
@@ -10,10 +11,18 @@ namespace TestProject1
     {        
         static void Main(string[] args)
         {
-            List<Product> products = new List<Product>() { new Product { ID = 1, Code = "B" }, new Product { ID = 2, Code = "A"}, new Product { ID = 3, Code = "C"} };
+            log4net.Config.XmlConfigurator.Configure();
+
+            List<Product> products = new List<Product>() { new Product { ID = 1, Code = "B" }, new Product { ID = 2, Code = "A"}, new Product { ID = 3, Code = "C"}, new Product { ID = 4, Code = "B"} };
+
             products.Sort();
+            products = products.Distinct<Product>(new ProductComparer()).ToList<Product>();
+
             foreach (var product in products)
-                Console.WriteLine(product.Code);            
+                Console.WriteLine(product.Code);
+                        
+            LogManager.GetLogger(typeof(Program)).Info("test log message");
+
             Console.ReadLine();
         }
     }
